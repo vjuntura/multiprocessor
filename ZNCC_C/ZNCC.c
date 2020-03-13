b057d4d38d40513530a199f46858b18daed69d72
vainojuntura <vaino.juntura@outlook.com>
2020-03-13 09:50:02 +0200

    Clean up and finalize

--- ZNCC_C/ZNCC.c
+++ ZNCC_C/ZNCC.c
@@ -6,25 +6,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <limits.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <time.h>
 
 
-#define windowX 21
-#define windowY 15
+#define windowX 20
+#define windowY 14
 
 void zncc(unsigned char* IL, unsigned char* IR,
           unsigned width, unsigned height,
           int Max_Disp, int Min_Disp,
           unsigned char* DisparityMap);
 
-
 void post_processing(unsigned char* IL, unsigned char* IR,
                       unsigned width, unsigned height,
                       int Max_Disp, unsigned size,
-                      unsigned char* result);
+                      unsigned char* resultMap);
 
 void convertgray(const uint8_t* IL, const uint8_t* IR,
                 uint8_t* grayL, uint8_t* grayR, uint32_t w, uint32_t h);
@@ -36,9 +34,9 @@ int main(int argc, char *argv[]) {
     const char* filenameL = "im0.png";
     const char* filenameR = "im1.png";
 
-    const char* out_filename = "testi.png";
-    const char* dimage1out_filename = "dimage1.png";
-    const char* dimage2out_filename = "dimage2.png";
+    const char* out_filename = "output.png";
+    const char* dimage1out_filename = "disparityMapL.png";
+    const char* dimage2out_filename = "disparityMapR.png";
 
     uint8_t* original_IL = 0;
     uint8_t* original_IR = 0;
@@ -61,7 +59,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    int max_disp = 64;
+    int max_disp = 60;
     int min_disp = 0;
 
     //New sizes after rescaling
@@ -78,41 +76,27 @@ int main(int argc, char *argv[]) {
     // Disparity maps
     uint8_t* DisparityMapL2R;
     uint8_t* DisparityMapR2L;
-    uint8_t* result;
+    uint8_t* resultMap;
 
     DisparityMapL2R = (uint8_t*)malloc(size * sizeof(uint8_t));
     DisparityMapR2L = (uint8_t*)malloc(size * sizeof(uint8_t));
-    result = (uint8_t*)malloc(size * sizeof(uint8_t));
+    resultMap = (uint8_t*)malloc(size * sizeof(uint8_t));
 
     // Calculate disparity maps with zncc
-
     // Left to right
     zncc(gray_IL, gray_IR, width, height, max_disp, min_disp, DisparityMapL2R);
-    /*printf("Disparity left\n");
-    for(int i=0; i < width1; i++){
-        for(int j=0; j < height1; j++){
-            printf("%d\n", DisparityMapL2R[i*height1 + j]);
-        }
-    }*/
-
     lodepng_encode_file(dimage1out_filename, DisparityMapL2R, width, height, LCT_GREY, bitdepth);
+
     // Right to left
     zncc(gray_IR, gray_IL, width, height, min_disp, -max_disp, DisparityMapR2L);
-    /*printf("Disparity right\n");
-    for(int i=0; i < width2; i++){
-        for(int j=0; j < height2; j++){
-            printf("%d\n", DisparityMapR2L[i*height2 + j]);
-        }
-    }*/
     lodepng_encode_file(dimage2out_filename, DisparityMapR2L, width, height, LCT_GREY, bitdepth);
 
-    post_processing(DisparityMapL2R, DisparityMapR2L, width, height, max_disp, size, result);
-
-    lodepng_encode_file(out_filename, result, width, height, LCT_GREY, bitdepth);
+    post_processing(DisparityMapL2R, DisparityMapR2L, width, height, max_disp, size, resultMap);
+    lodepng_encode_file(out_filename, resultMap, width, height, LCT_GREY, bitdepth);
 
     free(DisparityMapL2R);
     free(DisparityMapR2L);
-    free(result);
+    free(resultMap);
     free(original_IL);
     free(original_IR);
     free(gray_IL);
@@ -216,46 +200,38 @@ void zncc(uint8_t* IL, uint8_t* IR,
 void post_processing(uint8_t* IL, uint8_t* IR,
                       uint32_t width, uint32_t height,
                       int Max_Disp, uint32_t size,
-                      uint8_t* result) {
+                      uint8_t* resultMap) {
 
     int threshold = 12;
-    //unsigned char* tempMap = (unsigned char*)malloc(size*sizeof(unsigned char));
-
     int color_nearest = 0;
+    uint8_t max = 0;
+    uint8_t min = 255;
 
-    //TODO ei ehkä toimi, katso matlabista j-window
     //Simplest form of neigherest neighbour
     printf("Post processing...\n");
-/*    for (int i = 0; i < size; i++) {
-        if (abs(IL[i] - IR[i]) < threshold) {
-            result[i] = IL[i];
-            color_nearest = result[i];
-        } else {
-            result[i] = color_nearest;
-        }
-    }*/
-
-    for(int i=0; i < height; i++){
+    for(int i = 0; i < height; i++){
         color_nearest = 0;
-        for(int j=0; j < width; j++){
+        for(int j = 0; j < width; j++){
             if (abs(IL[i*width + j] - IR[i*width + j]) < threshold) {
-
-                result[i*width + j] = IL[i*width + j];
-
-                // // Exclude borders
-                // if (!(i + Win_X >= 0) || !(i + Win_X < height) ||
-                //     !(j + Win_Y >= 0) || !(j + Win_Y < width) ||
-                //     !(j + Win_Y - d  >= 0) || !(j + Win_Y - d < width)) {
-                //     continue;
-                // }
-                color_nearest = result[i*width + j];
+                resultMap[i*width + j] = IL[i*width + j];
+                color_nearest = resultMap[i*width + j];
             } else {
-                result[i*width + j] = color_nearest;
+                resultMap[i*width + j] = color_nearest;
             }
         }
     }
 
-    //Blur image for better representation only
+    //Perform normalization
+    for (uint32_t i = 0; i < size; i++) {
+        if (resultMap[i] > max)
+            max = resultMap[i];
+        if (resultMap[i] < min)
+            min = resultMap[i];
+    }
+
+    for (uint32_t i = 0; i < size; i++) {
+        resultMap[i] = (uint8_t) (255 * (resultMap[i] - min) / (max - min));
+    }
 
 }
 
